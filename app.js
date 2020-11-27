0fce3912687361b20b6e29b91720a665194c827b
sumukshashidhar <sumukuuu@gmail.com>
2020-11-27 23:28:55 +0530

    working nodejs app

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const os = require('os')
+const app = express();
+
+app.listen( "3000", "0.0.0.0", function() {
+    console.log(`Listening on 3000`)
+})
+
+app.get('/', function(req, res) {
+    res.send(`Hello From ${process.env.PORT || 3000} and ${os.hostname}`)
+})
